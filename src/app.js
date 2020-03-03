ff6e4017148484336a77a347336a8f6e24bd2f55
giovanniKleinCampigoto <giovannikleincampigoto@gmail.com>
2020-03-03 07:13:40 -0300

    Add prettier and started styling

--- src/app.js
+++ src/app.js
@@ -1,84 +1,124 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react'
 
-import { Window, App, Text, View, TouchableHighlight } from 'proton-native';
-import { exec } from 'child_process';
-
-const headerStyles = {
-  fontSize: '30px',
-  color: 'red',
-  marginLeft: '50px',
-};
+import {
+  Window,
+  App,
+  Text,
+  View,
+  TouchableHighlight,
+  TouchableWithoutFeedback,
+} from 'proton-native'
+import { exec } from 'child_process'
 
 const processStyle = {
-  height: '50px',
-};
+  marginLeft: '24px',
+}
 
 const buttonStyle = {
   backgroundColor: 'red',
-};
+  textAlign: 'center',
+}
+
+const headerStyles = {
+  backgroundColor: '#9ea6ac',
+  margin: '24px',
+}
 
 const Example = () => {
-  const [processData, setProcessData] = useState([]);
+  const [processData, setProcessData] = useState([])
 
   useEffect(() => {
     if (!processData.length) {
-      fetchRunningProcesses();
+      fetchRunningProcesses()
     }
-  }, []);
+  }, [])
 
   function fetchRunningProcesses() {
-    const command = `ps -o pid,user,%mem,%cpu,command ax | sort -b -k3 -r | awk '{print $1"|"$2"|"$3"|"$4"|"$5";"}'`;
+    const command = `ps -o pid,user,%mem,%cpu,command ax | sort -b -k3 -r | awk '{print $1"|"$2"|"$3"|"$4"|"$5";"}'`
 
     exec(command, (err, stdout, stderr) => {
-      const processes = [];
+      const processes = []
       if (err) {
-        console.error(err);
-        return;
+        console.error(err)
+        return
       }
 
-      const lines = stdout.split(';');
-      lines.shift();
+      const lines = stdout.split(';')
+      lines.shift()
 
       lines.forEach(data => {
-        const splittedData = data.split('|');
+        const splittedData = data.split('|')
         const parsedData = {
           PID: splittedData[0].trim(),
           user: splittedData[1],
           memory: splittedData[2],
           cpu: splittedData[3],
           name: splittedData[4],
-        };
-        processes.push(parsedData);
-      });
-      setProcessData(processes);
-    });
+        }
+        processes.push(parsedData)
+      })
+      setProcessData(processes)
+    })
+  }
+
+  function logPID(value) {
+    console.log(value)
   }
 
   function renderProcesses() {
-    console.log('akjshdkahjkd');
-    console.log(processData.length);
+    if (!processData.length) return
 
-    if (!processData.length) return;
-
-    return processData.map(data => {
+    return processData.map((data, index) => {
       return (
-        <View>
+        <View
+          style={{
+            padding: '24px',
+            flexDirection: 'row',
+            width: '100%',
+          }}
+          key={index}
+        >
+          <Text style={processStyle}>{data.PID}</Text>
+          <Text style={processStyle}>{data.user}</Text>
+          <Text style={processStyle}>{data.memory}</Text>
+          <Text style={processStyle}>{data.cpu}</Text>
           <Text style={processStyle}>{data.name}</Text>
-          <TouchableHighlight style={{ backgroundColor: 'white' }}>
+          <TouchableHighlight
+            style={{ backgroundColor: 'white', maxWidth: '100px' }}
+            onPress={() => logPID(data.PID)}
+          >
             <Text style={buttonStyle}>Terminate</Text>
           </TouchableHighlight>
         </View>
-      );
-    });
+      )
+    })
   }
 
   return (
     <App>
-      <Window style={{ width: 700, height: 768, backgroundColor: 'white' }}>
+      <Window
+        style={{
+          minWidth: 700,
+          minHeight: 768,
+          backgroundColor: 'white',
+        }}
+      >
+        <View
+          style={{
+            flexDirection: 'row',
+            background: '#9ea6ac',
+          }}
+        >
+          <Text style={headerStyles}>PID</Text>
+          <Text style={headerStyles}>User</Text>
+          <Text style={headerStyles}>Memory %</Text>
+          <Text style={headerStyles}>CPU %</Text>
+          <Text style={headerStyles}>Process Name</Text>
+        </View>
         {renderProcesses()}
       </Window>
     </App>
-  );
-};
+  )
+}
 
-export default Example;
+export default Example
